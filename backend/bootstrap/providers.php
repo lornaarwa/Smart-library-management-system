70bbe5af68693a9f32c1453f377a7eba1952eaf7
Martina <1peresmartina@gmail.com>
2026-07-23 14:37:34 +0300

    Add backend/bootstrap/providers.php

---
+++ backend/bootstrap/providers.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\FinePaymentServiceProvider::class,
+    App\Providers\SearchCatalogProvider::class,
+    App\Providers\LibraryEventServiceProvider::class,
+    App\Providers\AiChatbotServiceProvider::class,
+];
